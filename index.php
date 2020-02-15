dd40ce9875025b4f56db70251cc1667b3ad38c80
ayogaan <ayogaan@gmail.com>
2020-02-15 08:24:57 -0500

    home page of my browser

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	 <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet"> 
+	<script src="https://kit.fontawesome.com/77d256aa17.js" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Oswald&display=swap" rel="stylesheet"> 
+
+	<title></title>
+</head>
+<body>
+	<div class="main-page">
+		
+	
+	<div class="tag">im not Pro,<br>
+just an <span class="red">Enthusiast</span>
+<div class="quote">“Make voyages. Attempt them. There's nothing else.” </div>
+
+</div>
+	<div class="myphoto grid-container">
+		
+  <div class="grid-item">About me</div>
+  <div class="grid-item"><span class="sosmed">Skill</span><span class="account">don't doubt<br>it!</span></div>
+  <div class="grid-item">My Desktop</div>
+  
+  <div class="grid-item"><span class="sosmed">GitHub</span><span class="account"><i class="fab fa-github-alt"></i> ayogaan</span></div>  
+  <div class="grid-item"><span class="sosmed">Instagram</span><span class="account"><i class="fab fa-instagram"></i> arfanyoga</div>
+  
+  <div class="grid-item">Google</div>   
+</div>
+
+	</div>
+</body>
+</html>
+
